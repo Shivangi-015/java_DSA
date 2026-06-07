2dcc6a5c98542b54ba84a05ed538d675c61abac2
Shivangi Bansal <shivangi.bansal01@gmail.com>
2026-06-07 23:28:02 +0530

    07/06/26

---
+++ src/trees/BinaryTreeRepresentation.java
@@ -0,0 +1,22 @@
+package trees;
+
+ class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+public class BinaryTreeRepresentation {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.right.left = new Node(5);
+        System.out.println(root.data);
+    }
+}
