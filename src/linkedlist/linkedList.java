52c056dbaa1a738815f12785896ee7b945f5cc0c
Shivangi Bansal <shivangi.bansal01@gmail.com>
2026-06-20 22:45:36 +0530

    20/06/26

---
+++ src/linkedlist/linkedList.java
@@ -0,0 +1,21 @@
+package linkedlist;
+class Node {      //for implementing linkedlist we have to declare a self defined datatype
+    int data;
+    Node next;
+    //constructor
+    Node(int data1, Node next1){
+        this.data = data1;
+        this.next = next1;
+    }
+    Node(int data1){
+        this.data = data1;
+        this.next = null;
+    }
+}
+public class linkedList {
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3, 4, 5};
+        Node node1 = new Node(nums[3]);
+        System.out.println(node1.data);
+    }
+}
