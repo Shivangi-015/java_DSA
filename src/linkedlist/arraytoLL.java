2ac52de7bb4ebe0540ec94e3fbd13cd3230c4366
Shivangi-015 <shivangi.bansal01@gmail.com>
2026-06-27 00:28:07 +0530

    27/06/26

--- src/linkedlist/arraytoLL.java
+++ src/linkedlist/arraytoLL.java
@@ -1,4 +1,5 @@
 package linkedlist;
+
 class Node {      //for implementing linkedlist we have to declare a self defined datatype
     int data;
     Node next;
@@ -44,6 +45,94 @@ public class arraytoLL {
         }
         return 0;
     }
+    public static Node removeTail (Node head){
+        Node temp = head;
+        if(head == null || head.next == null){
+            return null;
+        }
+        while(temp.next.next != null){
+            temp = temp.next;
+        }
+         temp.next = null;
+        return head;
+    }
+    public static Node deleteAtK(Node head, int k){
+        if(head == null){
+            return head;
+        }
+        if(k==1){
+            head = head.next;
+            return head;
+        }
+        int count = 0;
+        Node temp = head;
+        Node prev = null;
+        while(temp!= null){
+            count++;
+            if(count == k){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+    public static Node deleteEl(Node head, int el){
+        if(head == null){
+            return head;
+        }
+        if(head.data == el){
+            head = head.next;
+            return head;
+        }
+        Node temp = head;
+        Node prev = null;
+        while(temp!= null){
+            if(temp.data == el){
+                prev.next = prev.next.next;
+                break;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return head;
+    }
+    public static Node insertionAtTail(Node head, int val){
+        if(head == null){
+            return new Node(val);
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        Node newNode = new Node(val);
+        temp.next = newNode;
+        return head;
+    }
+    public static Node insertionAtPositionK(Node head, int element, int k){
+        if (head == null){
+            if(k==1){
+                return new Node(element);
+            }
+            else{
+                return head;
+            }
+        }
+        int count = 0;
+        Node temp = head;
+        while(temp.next != null){
+            count++;
+            if(count == k-1){
+                Node newN = new Node(element);
+                newN.next = temp.next;
+                temp.next = newN;
+                break;
+            }
+            temp = temp.next;
+        }
+        return head;
+    }
     public static void main(String[] args){
         int [] arr = {12,11,10,13};
         Node head = convertArraytoLL(arr);
@@ -55,6 +144,14 @@ public class arraytoLL {
 //            temp = temp.next;
 //        }
 //        System.out.println(lengthofLL(head));
-        System.out.println(searchInLL(head , 15));
+        //System.out.println(searchInLL(head , 15));
+        //head = deleteEl(head, 11);
+       // head = new Node(100, head); inserting at head
+        //head = insertionAtTail(head, 21);
+        head = insertionAtPositionK(head, 44, 4);
+        while(head != null){
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
     }
 }
